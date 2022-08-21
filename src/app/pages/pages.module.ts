b3d882100e6c6614a973ff47bb5d3c3ed5e0f095
Justin <khanjal@gmail.com>
2022-08-21 09:08:44 -0400

    Initial code commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { ShiftsComponent } from './shifts/shifts.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ShiftsComponent
+  ],
+  imports: [],
+  providers: [],
+  bootstrap: []
+})
+export class PagesModule { }
