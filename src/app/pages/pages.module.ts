a1f16b58c3eb3b9d4f5c34d3250aff2e96722bf5
Justin <khanjal@gmail.com>
2023-03-06 13:26:39 -0500

    Started working on forms.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ShiftsComponent } from './shifts/shifts.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ShiftsComponent
   ],
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: []
 })
--- src/app/pages/shifts/shifts.component.ts
+++ src/app/pages/shifts/shifts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-shifts',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shifts.component.scss']
 })
 export class ShiftsComponent implements OnInit {
+
+  shiftControl = new FormControl('');
+  timeControl = new FormControl('');
+  serviceControl = new FormControl('');
+  placeControl = new FormControl('');
+  amountControl = new FormControl('');
 
   constructor() { }
 
