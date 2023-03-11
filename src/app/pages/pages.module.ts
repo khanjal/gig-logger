e7aab9c8d84c2f6924e78dcbae5f27412a4f6251
Justin <khanjal@gmail.com>
2023-03-11 12:43:16 -0500

    Trying to make it work.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { ShiftsComponent } from './shifts/shifts.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   ],
   imports: [
     MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
     ReactiveFormsModule
   ],
   providers: [],
