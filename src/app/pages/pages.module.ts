4c8436d7769474daf725cd027062e5a9431a86de
Justin <khanjal@gmail.com>
2023-03-11 23:27:56 -0500

    More google sheets work.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ShiftsComponent } from './shifts/shifts.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,10 +15,13 @@ import { MatInputModule } from '@angular/material/input';
     ShiftsComponent
   ],
   imports: [
+    CommonModule,
+    MatAutocompleteModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatListModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: []
--- src/app/pages/shifts/shifts.component.ts
+++ src/app/pages/shifts/shifts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { GoogleDriveService } from 'src/app/shared/services/googleSheet.service';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-shifts',
@@ -9,19 +11,73 @@ import { GoogleDriveService } from 'src/app/shared/services/googleSheet.service'
 })
 export class ShiftsComponent implements OnInit {
 
+  addressControl = new FormControl('');
   shiftControl = new FormControl('');
   timeControl = new FormControl('');
   serviceControl = new FormControl('');
   placeControl = new FormControl('');
   amountControl = new FormControl('');
 
+  isNewShift: boolean = false;
+
+  addresses: string[] = [];
+  filteredAddresses: Observable<string[]> | undefined;
+  shifts: string[] = [];
+
   constructor(private _googleSheetService: GoogleDriveService) { }
 
   async ngOnInit(): Promise<void> {
-      let testData = await this._googleSheetService.loadAddresses();
+    await this._loadAddresses();
+    await this._loadShifts();
+    
+    //console.log(testData);
 
-      console.log('Test');
-      //console.log(testData);
+    this.filteredAddresses = this.addressControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterAddress(value || '')),
+    );
   }
 
+  public onShiftSelected(value:string) {
+    if (value == 'new') {
+      this.isNewShift = true;
+    }
+    else {
+      this.isNewShift = false;
+    }
+  }
+
+  private _filterAddress(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.addresses.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  private async _loadAddresses() {
+    let addresses: string[] = [];
+    let addressData = localStorage.getItem('addresses') ?? '""';
+    this.addresses = JSON.parse(addressData);
+
+    if (!this.addresses) {
+      await this._googleSheetService.loadAddresses();
+      addressData = localStorage.getItem('addresses') ?? "''";
+      this.addresses = JSON.parse(addressData);
+    }
+
+    console.log(addresses);
+  }
+
+  private async _loadShifts() {
+    let shifts: string[] = [];
+    let shiftData = localStorage.getItem('shifts') ?? '""';
+    this.shifts = JSON.parse(shiftData);
+
+    if (!this.shifts) {
+      await this._googleSheetService.loadShifts();
+      shiftData = localStorage.getItem('shifts') ?? "''";
+      this.shifts = JSON.parse(shiftData);
+    }
+
+    console.log(shifts);
+  }
 }
