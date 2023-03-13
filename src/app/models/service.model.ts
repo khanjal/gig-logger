20c44fa572b7482545d8a0b38740ff6f80f94ea2
Justin <khanjal@gmail.com>
2023-03-13 15:06:05 -0400

    Refactored service calls. Added services drop down.

---
+++ src/app/models/service.model.ts
@@ -0,0 +1,5 @@
+export class ServiceModel {
+    id: number = 0;
+    service: string = "";
+    visits: number = 0;
+}
---
+++ src/app/shared/services/name.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core";
+import { NameModel } from "src/app/models/name.model";
+import { GoogleDriveService } from "./googleSheet.service";
+
+const sheetId = 1952572215;
+
+@Injectable()
+export class NameService {
+
+    constructor(private _googleSheetService: GoogleDriveService) { }
+    
+    public async getNames(): Promise<NameModel[]> {
+        let names: NameModel[] = [];
+        let nameData = localStorage.getItem('names') ?? '""';
+        names = JSON.parse(nameData);
+    
+        if (!names) {
+            await this.loadNames();
+            nameData = localStorage.getItem('names') ?? "''";
+            names = JSON.parse(nameData);
+        }
+    
+        // console.log(names);
+
+        return names;
+    }
+
+    public async loadNames() {
+        let sheet = await this._googleSheetService.getSheetData(sheetId);
+
+        console.log(sheet.title);
+        console.log(sheet.rowCount);
+
+        let rows = await sheet.getRows();
+        let names: NameModel[] = [];
+
+        rows.forEach(row => {
+            // console.log(row);
+            // console.log(row.rowIndex);
+            let nameModel: NameModel = new NameModel;
+            nameModel.id = row.rowIndex;
+            nameModel.name = row['Name'];
+            nameModel.addresses =  row['Addresses']?.split("; ");
+            nameModel.visits = row['Visits'];
+            // console.log(nameModel);
+
+            if (nameModel.name) {
+                names.push(nameModel);
+            }
+            
+        });
+        // console.log(names);
+        console.log(names.length);
+        // console.log(names);
+
+        // Load addresses into storage
+        localStorage.setItem('names', JSON.stringify(names));
+    }
+}
