5e3a209f5eb0fdbc2d56ced5dc98daceb8579e3b
Justin <khanjal@gmail.com>
2023-03-13 11:42:57 -0400

    Added models and more form updates

---
+++ src/app/models/address.model.ts
@@ -0,0 +1,7 @@
+export class AddressModel {
+    id: number = 0;
+    address: string = "";
+    names: string[] = [];
+    short: string = "";
+    visits: number = 0;
+}
--- src/app/models/name.model.ts
+++ src/app/models/name.model.ts
@@ -1,4 +1,5 @@
 export class NameModel {
+    id: number = 0;
     name: string = "";
     addresses: string[] = [];
     visits: number = 0;
