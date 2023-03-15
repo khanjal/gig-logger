c331dcc1d1bf99474190de5fc14d678d8e3d1421
Justin <khanjal@gmail.com>
2023-03-15 15:36:31 -0400

    Upgraded to mat buttons.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { ShiftsComponent } from './shifts/shifts.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,6 +22,7 @@ import { QuickComponent } from './quick/quick.component';
   imports: [
     CommonModule,
     MatAutocompleteModule,
+    MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
     MatListModule,
