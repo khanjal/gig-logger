7bd4b92fa50a7de861ab6c3065980769e55538c6
Justin <khanjal@gmail.com>
2023-03-15 13:31:59 -0400

    Added saved properties and added table to quick html

--- src/app/models/shift.model.ts
+++ src/app/models/shift.model.ts
@@ -2,6 +2,7 @@ export class ShiftModel {
     id: number = 0;
     date: string = "";
     key: string = "";
+    saved: boolean = false;
     service: string = "";
     shiftNumber: string = "";
 }
