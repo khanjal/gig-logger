e3a46fbe07a37e6850f290183d1236fbfb7599d5
Justin <khanjal@gmail.com>
2023-03-21 16:29:00 -0400

    Using numbers for number data

--- src/app/shared/models/trip.model.ts
+++ src/app/shared/models/trip.model.ts
@@ -1,18 +1,17 @@
 export class TripModel {
     id: number = 0;
     address: string = "";
-    amount: string = "";
-    cash: string = "";
+    cash: number = 0;
     date: string = "";
     key: string = "";
     name: string = "";
     note: string = "";
-    pay: string = "";
+    pay: number = 0;
     place: string = "";
     saved: boolean = false;
     service: string = "";
-    shiftNumber: string = "";
+    shiftNumber: number = 0;
     time: string = "";
-    tip: string = "";
-    total: string = "";
+    tip: number = 0;
+    total: number = 0;
 }
