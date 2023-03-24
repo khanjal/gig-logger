4479482c1d1a1a5b0f81aca5cfcc316585e4737f
Justin <khanjal@gmail.com>
2023-03-23 22:48:14 -0400

    First google login version added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { QuickComponent } from './pages/quick/quick.component';
 import { ShiftsComponent } from './pages/shifts/shifts.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'quick', component: QuickComponent },
   { path: 'shifts', component: ShiftsComponent }
 ];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,42 @@
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// https://github.com/abacritt/angularx-social-login
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  socialUser!: SocialUser;
+  isLoggedin: boolean = false;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService
+  ) {}
+  
+  ngOnInit(): void {
+
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+      console.log(this.socialUser);
+    });
+  }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  logOut(): void {
+    this.socialAuthService.signOut();
+  }
+}
