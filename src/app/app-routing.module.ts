9aa1d3cc5f17857c3911464b645849b94ef47916
Justin <khanjal@gmail.com>
2023-03-30 15:45:11 -0400

    Moved login to page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ProfileComponent } from './shared/components/profile/profile.component';
 import { QuickComponent } from './pages/quick/quick.component';
 import { ShiftsComponent } from './pages/shifts/shifts.component';
 import { AuthGuardService } from './shared/services/auth-guard.service';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'quick', component: QuickComponent, canActivate: [AuthGuardService] },
   { path: 'shifts', component: ShiftsComponent }
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
+import { SocialUser, SocialAuthService } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  socialUser!: SocialUser;
+  isLoggedin: boolean = false;
 
+  constructor(
+    private socialAuthService: SocialAuthService, public router: Router
+  ) {}
+  
+  async ngOnInit(): Promise<void> {
+      // console.log('Logged out');
+      this.socialAuthService.authState.subscribe((user) => {
+        this.socialUser = user;
+        this.isLoggedin = user != null;
+        sessionStorage.setItem('token', this.socialUser?.idToken);
+        // console.log(this.socialUser);
+    });
+  }
 }
