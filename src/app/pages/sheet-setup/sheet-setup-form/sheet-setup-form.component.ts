14f150c5bfd85d03b7027137cb9bee08ff53381e
Justin <khanjal@gmail.com>
2023-03-31 14:05:07 -0400

    Initial sheet setup form

--- src/app/pages/sheet-setup/sheet-setup-form/sheet-setup-form.component.ts
+++ src/app/pages/sheet-setup/sheet-setup-form/sheet-setup-form.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-sheet-setup-form',
+  selector: 'sheet-setup-form',
   templateUrl: './sheet-setup-form.component.html',
   styleUrls: ['./sheet-setup-form.component.scss']
 })
 export class SheetSetupFormComponent {
+  sheetForm = new FormGroup({
+    sheetid: new FormControl('')
+  });
 
+  public addSheet() {
+
+  }
 }
