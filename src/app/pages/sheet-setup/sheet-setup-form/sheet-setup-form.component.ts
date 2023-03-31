8df874ed5b129047736ef2923923ac7a456712ca
Justin <khanjal@gmail.com>
2023-03-31 13:40:15 -0400

    Adding sheet setup components

---
+++ src/app/pages/sheet-setup/sheet-setup-form/sheet-setup-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sheet-setup-form',
+  templateUrl: './sheet-setup-form.component.html',
+  styleUrls: ['./sheet-setup-form.component.scss']
+})
+export class SheetSetupFormComponent {
+
+}
