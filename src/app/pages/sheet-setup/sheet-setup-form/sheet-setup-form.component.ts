e12b75b392f4fc42ad422b0d90bbcfbfa14d7e76
Justin <khanjal@gmail.com>
2023-04-09 09:55:00 -0400

    Added spreadsheet management

--- src/app/pages/sheet-setup/sheet-setup-form/sheet-setup-form.component.ts
+++ src/app/pages/sheet-setup/sheet-setup-form/sheet-setup-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Spreadsheet } from '@models/spreadsheet.model';
+import { SpreadsheetService } from '@services/spreadsheet.service';
 import { GoogleSheetService } from 'src/app/shared/services/googleSheet.service';
 
 @Component({
@@ -8,6 +10,7 @@ import { GoogleSheetService } from 'src/app/shared/services/googleSheet.service'
   styleUrls: ['./sheet-setup-form.component.scss']
 })
 export class SheetSetupFormComponent {
+  @Output("parentReload") parentReload: EventEmitter<any> = new EventEmitter();
   
   sheetForm = new FormGroup({
     sheetId: new FormControl('')
@@ -16,7 +19,8 @@ export class SheetSetupFormComponent {
   saving: boolean = false;
 
   constructor(
-    private _googleService: GoogleSheetService
+    private _googleService: GoogleSheetService,
+    private _spreadsheetService: SpreadsheetService
   ) { }
 
   public async addSheet() {
@@ -33,11 +37,22 @@ export class SheetSetupFormComponent {
 
     if(sheetTitle != "") {
       console.log(sheetTitle);
+      let spreadsheet = new Spreadsheet();
+      spreadsheet.id = spreadsheetId;
+      spreadsheet.name = sheetTitle;
+      spreadsheet.default = "false";
 
-      // TODO: Verify all the sheets exist
+      // Check for default spreadsheet
+      let defaultSpreadsheet = (await this._spreadsheetService.querySpreadsheets("default", "true"))[0];
+
+      if (!defaultSpreadsheet?.id || defaultSpreadsheet.id === spreadsheet.id) {
+        spreadsheet.default = "true";
+      }
+
       // Add sheet & load data from it.
+      await this._spreadsheetService.update(spreadsheet);
 
-      await this._googleService.loadRemoteData(spreadsheetId);
+      await this._googleService.loadRemoteData();
 
       // TODO: Show sheet in list to remove (delete data)
     }
@@ -47,5 +62,8 @@ export class SheetSetupFormComponent {
 
     this.saving = false;
     this.sheetForm.reset();
+    this.parentReload.emit();
   }
 }
+
+
--- src/app/pages/sheet-setup/sheet-setup.component.ts
+++ src/app/pages/sheet-setup/sheet-setup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ISpreadsheet } from '@interfaces/spreadsheet.interface';
+import { GoogleSheetService } from '@services/googleSheet.service';
+import { SpreadsheetService } from '@services/spreadsheet.service';
 
 @Component({
   selector: 'app-sheet-setup',
@@ -6,6 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./sheet-setup.component.scss']
 })
 export class SheetSetupComponent {
+
+  reloading: boolean = false;
+  setting: boolean = false;
+  spreadsheets: ISpreadsheet[] | undefined;
+
+  constructor(
+    private _googleSheetService: GoogleSheetService,
+    private _spreadsheetService: SpreadsheetService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.load();
+  }
+
+  public async load() {
+    this.spreadsheets = await this._spreadsheetService.getSpreadsheets();
+  }
+
+  public async reload() {
+    this.reloading = true;
+    await this._googleSheetService.loadRemoteData();
+    this.reloading = false;
+  }
+
+  public async setDefault(spreadsheet: ISpreadsheet) {
+    this.setting = true;
+    // Make current default not default
+    let defaultSpreadsheet = (await this._spreadsheetService.querySpreadsheets("default", "true"))[0];
+    defaultSpreadsheet.default = "false";
+    await this._spreadsheetService.update(defaultSpreadsheet);
+
+    spreadsheet.default = "true";
+    await this._spreadsheetService.update(spreadsheet);
+    this.load();
+    this.reload();
+    this.setting = false;
+  }
 
   public getDataSize() {
     // return LocalStorageHelper.formatBytes(LocalStorageHelper.getDataSize());
