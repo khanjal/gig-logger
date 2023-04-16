a6c5d07aa6fb6d1a8705463ecfa75768d19b9f28
Justin <khanjal@gmail.com>
2023-04-16 11:11:52 -0400

    Added weekdays table. Fixed trip pickup & start address bug.

---
+++ src/app/shared/interfaces/weekday.interface.ts
@@ -0,0 +1,11 @@
+export interface IWeekday {
+    id: number;
+    day: string;
+    trips: number;
+    pay: number;
+    tips: number;
+    bonus: number;
+    cash: number;
+    total: number;
+    days: number;
+}
