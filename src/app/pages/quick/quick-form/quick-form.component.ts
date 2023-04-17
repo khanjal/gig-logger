aac880ee8fe59efaed1415b1518f4d1e5c65362c
Justin <khanjal@gmail.com>
2023-04-16 22:06:59 -0400

    Bug fix for no local shifts for default shift.

--- src/app/pages/quick/quick-form/quick-form.component.ts
+++ src/app/pages/quick/quick-form/quick-form.component.ts
@@ -209,7 +209,9 @@ export class QuickFormComponent implements OnInit {
 
       this.shifts = ShiftHelper.sortShiftsDesc(this.shifts);
 
-      let trips = await this._tripService.queryLocalTrips("date", new Date().toLocaleDateString());
+      let trips = [... await this._tripService.queryLocalTrips("date", new Date().toLocaleDateString()),
+                  ...await this._tripService.queryRemoteTrips("date", new Date().toLocaleDateString())];
+
       let latestTrip = TripHelper.sortTripsDesc(trips)[0];
       let shift = this.shifts.find(x => x.key === latestTrip?.key);
 
