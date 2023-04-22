9b73f1de5fdd7d109a6cfc32154f441a4d257b97
Justin <khanjal@gmail.com>
2023-04-21 22:29:34 -0400

    Added focus-scroll directive

---
+++ src/app/shared/directives/focus-scroll/focus-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FocusScrollDirective } from './focus-scroll.directive';
+
+describe('FocusScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FocusScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
