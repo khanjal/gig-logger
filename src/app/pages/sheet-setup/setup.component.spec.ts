ca7b31e3436c27406ef269e9a9782ae3a4e2309d
Justin <khanjal@gmail.com>
2023-04-23 08:40:06 -0400

    Cleanup and styling updates.

--- src/app/pages/sheet-setup/setup.component.spec.ts
+++ src/app/pages/sheet-setup/setup.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SheetSetupComponent } from './setup.component';
+import { SetupComponent } from './setup.component';
 
 describe('SheetSetupComponent', () => {
-  let component: SheetSetupComponent;
-  let fixture: ComponentFixture<SheetSetupComponent>;
+  let component: SetupComponent;
+  let fixture: ComponentFixture<SetupComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SheetSetupComponent ]
+      declarations: [ SetupComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SheetSetupComponent);
+    fixture = TestBed.createComponent(SetupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
