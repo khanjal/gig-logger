22c2bfc4245ac837969642fa2cd8eff0f17da67b
Justin <khanjal@gmail.com>
2023-05-02 11:23:37 -0400

    Added notes and number of visits to nameAddresses and addressNames

--- src/app/shared/models/address.model.ts
+++ src/app/shared/models/address.model.ts
@@ -1,6 +1,10 @@
-export class AddressModel {
+import { IAddress } from "@interfaces/address.interface";
+import { IName } from "@interfaces/name.interface";
+
+export class AddressModel implements IAddress {
     id: number = 0;
     address: string = "";
-    names: string[] = [];
+    names: IName[] = [];
+    notes: string[] = [];
     visits: number = 0;
 }
