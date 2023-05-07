276b85d04d3fc697c9a5e531044252970cf58b5d
Justin <khanjal@gmail.com>
2023-05-06 20:04:14 -0400

    Added bonus/cash/pay/tip/total interface.

--- src/app/shared/models/shift.model.ts
+++ src/app/shared/models/shift.model.ts
@@ -2,14 +2,18 @@ import { IShift } from "@interfaces/shift.interface";
 
 export class ShiftModel implements IShift {
     id?: number;
+    bonus: number = 0;
+    cash: number = 0;
     date: string = new Date().toLocaleDateString();;
     distance: number = 0;
     end: string = "";
     key: string = "";
+    pay: number = 0;
     saved: string = "false";
     service: string = "";
     number: number = 0;
     start: string = "";
+    tip: number = 0;
     total: number = 0;
     trips: number = 0;
 }
