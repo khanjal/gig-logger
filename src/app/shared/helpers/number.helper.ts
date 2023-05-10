a173681deac5286c0b93880cd49948a059c9b3d5
Justin <khanjal@gmail.com>
2023-05-09 21:59:00 -0400

    Updated helper to return 0 if it's NaN

--- src/app/shared/helpers/number.helper.ts
+++ src/app/shared/helpers/number.helper.ts
@@ -1,5 +1,6 @@
 export class NumberHelper {
     static getNumberFromString(numberString: string = ""): number {
-        return Number(numberString.replace(/[^0-9.-]+/g,""));
+        let number = Number(numberString.replace(/[^0-9.-]+/g,""));
+        return isNaN(number) ? 0 : number;
     }
 }
