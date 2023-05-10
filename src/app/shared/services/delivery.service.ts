8694cd4a3941046dc93620b8eb24dc4e9375a807
Justin <khanjal@gmail.com>
2023-05-10 15:45:12 -0400

    Added deliveries table and loading.

---
+++ src/app/shared/services/delivery.service.ts
@@ -0,0 +1,20 @@
+import { liveQuery } from 'dexie';
+import { spreadsheetDB } from '@data/spreadsheet.db';
+import { IDelivery } from '@interfaces/delivery.interface';
+
+export class DeliveryService {
+    names$ = liveQuery(() => spreadsheetDB.deliveries.toArray());
+
+    public async getRemoteDeliveries(): Promise<IDelivery[]> {
+        return await spreadsheetDB.deliveries.toArray();
+    }
+
+    public async loadDeliveries(deliveries: IDelivery[]) {
+        await spreadsheetDB.deliveries.clear();
+        await spreadsheetDB.deliveries.bulkAdd(deliveries);
+    }
+
+    public async queryRemoteShifts(field: string, value: string | number): Promise<IDelivery[]> {
+        return await spreadsheetDB.deliveries.where(field).equals(value).toArray();
+    }
+}
