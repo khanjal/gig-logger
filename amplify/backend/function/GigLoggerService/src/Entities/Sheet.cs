4416601750c65c20694e3543eea58507a1c9dedd
Justin <khanjal@gmail.com>
2023-05-27 13:18:01 -0400

    Started pulling from google sheets.

---
+++ amplify/backend/function/GigLoggerService/src/Entities/Sheet.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+public class Sheet {
+    public List<Shift> Shifts { get; set; }
+    public List<Trip> Trips { get; set; }
+}
--- amplify/backend/function/GigLoggerService/src/GigLoggerService.cs
+++ amplify/backend/function/GigLoggerService/src/GigLoggerService.cs
@@ -7,10 +7,13 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using Google.Apis.Sheets.v4;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
 
 // https://aws.amazon.com/blogs/developer/introducing-net-core-support-for-aws-amplify-backend-functions/
 // https://code-maze.com/google-sheets-api-with-net-core/
 // https://github.com/awsdocs/aws-lambda-developer-guide/tree/main/sample-apps/blank-csharp/src/blank-csharp
+// https://nodogmablog.bryanhogan.net/2022/10/dependency-injection-with-the-lambda-annotations-library-for-net-part-1-lambda-applications/
+// https://blog.steadycoding.com/using-singletons-in-net-core-in-aws-lambda/
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -23,14 +26,18 @@ namespace GigLoggerService
   // to match if you intend to test the function with 'amplify mock function'
   public class GigLoggerService
     {
-
-        private static ServiceProvider ServiceProvider { get; set; }
+        readonly ServiceProvider _serviceProvider;
+        public SpreadsheetsResource.ValuesResource _googleSheetValues;
+        public Sheet _sheet = new();
+        public string _spreadsheetId = "";
 
         public GigLoggerService()
         {
-            var services = new ServiceCollection();
-            ConfigureServices(services);
-            ServiceProvider = services.BuildServiceProvider();
+            Console.WriteLine("Setting up the DI container");
+            var serviceCollection = new ServiceCollection();
+            Console.WriteLine("Adding a scoped service");
+            serviceCollection.AddScoped(typeof(GoogleSheetsHelper));
+            _serviceProvider = serviceCollection.BuildServiceProvider();
         }
 
         /// <summary>
@@ -43,7 +50,7 @@ namespace GigLoggerService
         /// to match if you intend to test the function with 'amplify mock function'
         /// </remarks>
 #pragma warning disable CS1998
-        public async Task<APIGatewayProxyResponse> LambdaHandler(APIGatewayProxyRequest request, ILambdaContext context, GoogleSheetsHelper googleSheetsHelper)
+        public async Task<APIGatewayProxyResponse> LambdaHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
             var response = new APIGatewayProxyResponse {
                 Headers = new Dictionary<string, string> {
@@ -52,17 +59,29 @@ namespace GigLoggerService
                 }
             };
 
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var googleSheetsService = scope.ServiceProvider.GetRequiredService<GoogleSheetsHelper>();
+                _googleSheetValues = googleSheetsService.Service.Spreadsheets.Values;
+            }
+
+            // using var scope = ServiceProvider.CreateScope();
+            // await scope.ServiceProvider.GetService<GigLoggerService>().Run(evnt);
+
             string contentType = null;
             request.Headers?.TryGetValue("Content-Type", out contentType);
-            SpreadsheetsResource.ValuesResource _googleSheetValues;
 
             switch (request.HttpMethod) {
                 case "GET":
                     context.Logger.LogLine($"Get Request: {request.Path}\n");
-                    _googleSheetValues = googleSheetsHelper.Service.Spreadsheets.Values;
+                    _spreadsheetId = request.PathParameters["id"];
+
+                    _sheet.Shifts = GetShifts();
+                    _sheet.Trips = GetTrips();
 
                     response.StatusCode = (int)HttpStatusCode.OK;
-                    response.Body = "{ \"message\": \"Hello AWS Serverless " + request.PathParameters["id"] +" \" }";
+                    // response.Body = "{ \"message\": \"Hello AWS Serverless " + request.PathParameters["id"] +" \" }";
+                    response.Body = JsonSerializer.Serialize(_sheet);
                     response.Headers["Content-Type"] = "application/json";
                     break;
                 case "POST":
@@ -94,25 +113,47 @@ namespace GigLoggerService
             return response;
         }
 
+        private List<Trip> GetTrips() {
+            var range = "Trips";
+            var googleRequest = _googleSheetValues.Get(_spreadsheetId, range);
+            var googleResponse = googleRequest.Execute();
+            var values = googleResponse.Values;
+
+            var trips = TripsMapper.MapFromRangeData(values);
+
+            return trips;
+        }
+
+        private List<Shift> GetShifts() {
+            var range = "Shifts";
+            var googleRequest = _googleSheetValues.Get(_spreadsheetId, range);
+            var googleResponse = googleRequest.Execute();
+            var values = googleResponse.Values;
+
+            var shifts = ShiftsMapper.MapFromRangeData(values);
+
+            return shifts;
+        }
+
         /// <summary>
         /// Configure whatever dependency injection you like here
         /// </summary>
         /// <param name="services"></param>
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            // add dependencies here ex: Logging, IMemoryCache, Interface mapping to concrete class, etc...
+        // private static void ConfigureServices(IServiceCollection services)
+        // {
+        //     // add dependencies here ex: Logging, IMemoryCache, Interface mapping to concrete class, etc...
 
-            // add a hook to your class that will actually do the application logic
-            services.AddTransient<GigLoggerService>();
-        }
+        //     // add a hook to your class that will actually do the application logic
+        //     services.AddTransient<GigLoggerService>();
+        // }
 
         /// <summary>
         /// Since we don't want to dispose of the ServiceProvider in the FunctionHandler, we will
         /// at least try to clean up after ourselves in the destructor for the class.
         /// </summary>
-        ~GigLoggerService()
-        {
-            ServiceProvider.Dispose();
-        }
+        // ~GigLoggerService()
+        // {
+        //     ServiceProvider.Dispose();
+        // }
     }
 }
