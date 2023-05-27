4add312afbe7f7b331a16a76742a2edd50c3fffa
Justin <khanjal@gmail.com>
2023-05-26 23:38:05 -0400

    Started working on DI and a service.

--- amplify/backend/function/GigLoggerService/src/GigLoggerService.cs
+++ amplify/backend/function/GigLoggerService/src/GigLoggerService.cs
@@ -5,6 +5,12 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Google.Apis.Sheets.v4;
+using Microsoft.Extensions.DependencyInjection;
+
+// https://aws.amazon.com/blogs/developer/introducing-net-core-support-for-aws-amplify-backend-functions/
+// https://code-maze.com/google-sheets-api-with-net-core/
+// https://github.com/awsdocs/aws-lambda-developer-guide/tree/main/sample-apps/blank-csharp/src/blank-csharp
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -17,6 +23,16 @@ namespace GigLoggerService
   // to match if you intend to test the function with 'amplify mock function'
   public class GigLoggerService
     {
+
+        private static ServiceProvider ServiceProvider { get; set; }
+
+        public GigLoggerService()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
         /// <summary>
         /// A Lambda function to respond to HTTP Get methods from API Gateway
         /// </summary>
@@ -27,7 +43,7 @@ namespace GigLoggerService
         /// to match if you intend to test the function with 'amplify mock function'
         /// </remarks>
 #pragma warning disable CS1998
-        public async Task<APIGatewayProxyResponse> LambdaHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> LambdaHandler(APIGatewayProxyRequest request, ILambdaContext context, GoogleSheetsHelper googleSheetsHelper)
         {
             var response = new APIGatewayProxyResponse {
                 Headers = new Dictionary<string, string> {
@@ -38,10 +54,13 @@ namespace GigLoggerService
 
             string contentType = null;
             request.Headers?.TryGetValue("Content-Type", out contentType);
+            SpreadsheetsResource.ValuesResource _googleSheetValues;
 
             switch (request.HttpMethod) {
                 case "GET":
                     context.Logger.LogLine($"Get Request: {request.Path}\n");
+                    _googleSheetValues = googleSheetsHelper.Service.Spreadsheets.Values;
+
                     response.StatusCode = (int)HttpStatusCode.OK;
                     response.Body = "{ \"message\": \"Hello AWS Serverless " + request.PathParameters["id"] +" \" }";
                     response.Headers["Content-Type"] = "application/json";
@@ -73,6 +92,27 @@ namespace GigLoggerService
             }
 
             return response;
+        }
+
+        /// <summary>
+        /// Configure whatever dependency injection you like here
+        /// </summary>
+        /// <param name="services"></param>
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // add dependencies here ex: Logging, IMemoryCache, Interface mapping to concrete class, etc...
+
+            // add a hook to your class that will actually do the application logic
+            services.AddTransient<GigLoggerService>();
+        }
+
+        /// <summary>
+        /// Since we don't want to dispose of the ServiceProvider in the FunctionHandler, we will
+        /// at least try to clean up after ourselves in the destructor for the class.
+        /// </summary>
+        ~GigLoggerService()
+        {
+            ServiceProvider.Dispose();
         }
     }
 }
