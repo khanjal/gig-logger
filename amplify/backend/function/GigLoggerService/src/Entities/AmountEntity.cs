7c8e0fbd43cc08970ef674cfb452309f0eb748c0
Justin <khanjal@gmail.com>
2023-06-08 12:41:44 -0400

    Made decimals nullable.

--- amplify/backend/function/GigLoggerService/src/Entities/AmountEntity.cs
+++ amplify/backend/function/GigLoggerService/src/Entities/AmountEntity.cs
@@ -3,18 +3,18 @@ using System.Text.Json.Serialization;
 public class AmountEntity
 {
     [JsonPropertyName("pay")]
-    public decimal Pay { get; set; }
+    public decimal? Pay { get; set; }
 
     [JsonPropertyName("tip")]
-    public decimal Tip { get; set; }
+    public decimal? Tip { get; set; }
 
     [JsonPropertyName("bonus")]
-    public decimal Bonus { get; set; }
+    public decimal? Bonus { get; set; }
 
     [JsonPropertyName("total")]
-    public decimal Total { get; set; }
+    public decimal? Total { get; set; }
 
     [JsonPropertyName("cash")]
-    public decimal Cash { get; set; }
+    public decimal? Cash { get; set; }
     
 }
