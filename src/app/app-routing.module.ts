3c7a5a7457c86231a916a8a3cfa8527cde90b3ec
Justin <khanjal@gmail.com>
2023-06-25 12:03:22 -0400

    Added uber calculator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { QuickComponent } from './pages/quick/quick.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SetupComponent } from './pages/sheet-setup/setup.component';
 import { DefaultSheetGuard } from '@guards/default-sheet/default-sheet.guard';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'calculator', component: CalculatorComponent },
   { path: 'login', component: LoginComponent },
   { path: 'quick', component: QuickComponent, canActivate: [DefaultSheetGuard] },
   { path: 'setup', component: SetupComponent },
