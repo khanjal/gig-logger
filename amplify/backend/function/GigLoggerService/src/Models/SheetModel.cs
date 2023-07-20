5fd2f37a716b94477612b7fd25395bbe3b5226c7
Justin <khanjal@gmail.com>
2023-07-19 23:33:19 -0400

    Started adding create sheet logic.

---
+++ amplify/backend/function/GigLoggerService/src/Models/SheetModel.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+public class SheetModel {
+    public string Name { get; set; }
+    public List<SheetHeaderModel> Headers { get; set; }
+    public string TabColor { get; set; }
+}
