acf20a722971cf6f4f56d831ef818399556c91fd
Justin <khanjal@gmail.com>
2023-07-26 23:53:21 -0400

    Started generating sheets within a spreadsheet.

--- amplify/backend/function/GigLoggerService/src/Models/SheetModel.cs
+++ amplify/backend/function/GigLoggerService/src/Models/SheetModel.cs
@@ -4,4 +4,7 @@ public class SheetModel {
     public string Name { get; set; }
     public List<SheetHeaderModel> Headers { get; set; }
     public ColorEnum TabColor { get; set; }
+    public bool ProtectSheet { get; set; }
+    public int FreezeColumnCount { get; set;}
+    public int FreezeRowCount { get; set; }
 }
