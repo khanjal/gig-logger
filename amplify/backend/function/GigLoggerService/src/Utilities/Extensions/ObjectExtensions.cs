8d4501b25637e0606d6dfd4d301f131cf967f1eb
Justin <khanjal@gmail.com>
2023-08-04 22:56:42 -0400

    Moved visit headers to helper.

--- amplify/backend/function/GigLoggerService/src/Utilities/Extensions/ObjectExtensions.cs
+++ amplify/backend/function/GigLoggerService/src/Utilities/Extensions/ObjectExtensions.cs
@@ -2,6 +2,11 @@ using System.Linq;
 
 public static class ObjectExtensions
 {
+    public static string GetColumn(this SheetModel sheet, HeaderEnum header)
+    {
+        return $"{sheet.Headers.FirstOrDefault(x => x.Name == header.DisplayName()).Column}";
+    }
+
     public static string GetRange(this SheetModel sheet, HeaderEnum header)
     {
         return $"{sheet.Name}!{sheet.Headers.FirstOrDefault(x => x.Name == header.DisplayName()).Range}";
