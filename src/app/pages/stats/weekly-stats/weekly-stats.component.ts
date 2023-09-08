275b1781a9236ab4b22b8adf2fbbc942ad88bb41
Justin <khanjal@gmail.com>
2023-09-08 14:00:39 -0400

    Initial page

---
+++ src/app/pages/stats/weekly-stats/weekly-stats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-stats',
+  templateUrl: './weekly-stats.component.html',
+  styleUrls: ['./weekly-stats.component.scss']
+})
+export class WeeklyStatsComponent {
+
+}
