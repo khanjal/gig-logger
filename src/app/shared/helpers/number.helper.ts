fc87695ec872577ccef030b66018b6334b43e83c
Justin <khanjal@gmail.com>
2023-09-27 14:19:59 -0400

    Added ability to see storage size of database.

--- src/app/shared/helpers/number.helper.ts
+++ src/app/shared/helpers/number.helper.ts
@@ -3,4 +3,16 @@ export class NumberHelper {
         let number = Number(numberString.replace(/[^0-9.-]+/g,""));
         return isNaN(number) ? 0 : number;
     }
+
+    static getDataSize(bytes: number = 0, decimals = 2) {
+        let units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+        let i = 0
+        
+        for (i; bytes > 1024; i++) {
+            bytes /= 1024;
+        }
+
+        return parseFloat(bytes.toFixed(decimals)) + ' ' + units[i]
+    }
 }
