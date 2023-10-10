67706fd6909b5ed233aca622e7312e77497e2ba0
Justin <khanjal@gmail.com>
2023-10-10 16:59:39 -0400

    More updates to dropdown.

--- src/app/pages/stats/stats.component.ts
+++ src/app/pages/stats/stats.component.ts
@@ -9,7 +9,7 @@ import { DateHelper } from '@helpers/date.helper';
 })
 export class StatsComponent implements OnInit {
   date: string = "all";
-  dates: string[] = [];
+  dates: {key: string, value: string}[] = [];
   selectedDate: string = "";
 
   statForm = new FormGroup({
@@ -27,23 +27,16 @@ export class StatsComponent implements OnInit {
 
   setupDropdown() {
     var date = new Date();
-    // Today
-    this.dates.push(DateHelper.getISOFormat(DateHelper.getDateFromDays()));
+    this.dates.push({key: "Today", value: DateHelper.getISOFormat(DateHelper.getDateFromDays())});
     // Yesterday
-    this.dates.push(DateHelper.getISOFormat(DateHelper.getDateFromDays(1)));
-    // This week
-    this.dates.push(DateHelper.getISOFormat(DateHelper.getMonday()));
+    // this.dates.push(DateHelper.getISOFormat(DateHelper.getDateFromDays(1)));
+    this.dates.push({key: "This Week", value: DateHelper.getISOFormat(DateHelper.getMonday())});
     // Last Week
     // this.dates.push(DateHelper.getISOFormat(DateHelper.getMonday(DateHelper.getDateFromDays(7))));
-    // This month
-    this.dates.push(DateHelper.getISOFormat(new Date(date.getFullYear(), date.getMonth(), 1)));
-    // console.log(thisMonth);
+    this.dates.push({key: "This Month", value: DateHelper.getISOFormat(new Date(date.getFullYear(), date.getMonth(), 1))});
     // last month
     var lastMonth = DateHelper.getISOFormat(new Date(date.getFullYear(), date.getMonth()-1, 1));
     // console.log(lastMonth);
-    // this year
-    this.dates.push(DateHelper.getISOFormat(new Date(date.getFullYear(), 0, 1)));
-    // console.log(thisYear);
-    // all
+    this.dates.push({key: "This Year", value: DateHelper.getISOFormat(new Date(date.getFullYear(), 0, 1))});
   }
 }
