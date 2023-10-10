7a2a4c36dd2c5b8ce80d7afc072944b22d52b425
Justin <khanjal@gmail.com>
2023-10-10 13:54:20 -0400

    Started setting up stats drop down

--- src/app/pages/stats/service-stats/service-stats.component.ts
+++ src/app/pages/stats/service-stats/service-stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateHelper } from '@helpers/date.helper';
 import { IService } from '@interfaces/service.interface';
 import { ServiceService } from '@services/service.service';
 import { ShiftService } from '@services/shift.service';
@@ -20,6 +21,30 @@ export class ServiceStatsComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     this.displayedColumns = ['service', 'trips', 'pay', 'tips', 'bonus', 'total', 'cash'];
     await this.getServices();
+
+    var date = new Date();
+    // Today
+    let today = DateHelper.getISOFormat(DateHelper.getDateFromDays());
+    console.log(today);
+    // Yesterday
+    let yesterday = DateHelper.getISOFormat(DateHelper.getDateFromDays(1));
+    console.log(yesterday);
+    // This week
+    let thisWeek = DateHelper.getISOFormat(DateHelper.getMonday());
+    console.log(thisWeek);
+    // Last Week
+    let lastWeek = DateHelper.getISOFormat(DateHelper.getMonday(DateHelper.getDateFromDays(7)));
+    console.log(lastWeek);
+    // This month
+    var thisMonth = DateHelper.getISOFormat(new Date(date.getFullYear(), date.getMonth(), 1));
+    console.log(thisMonth);
+    // last month
+    var lastMonth = DateHelper.getISOFormat(new Date(date.getFullYear(), date.getMonth()-1, 1));
+    console.log(lastMonth);
+    // this year
+    var thisYear = DateHelper.getISOFormat(new Date(date.getFullYear(), 0, 1));
+    console.log(thisYear);
+    // all
   }
 
   async getServices() {
