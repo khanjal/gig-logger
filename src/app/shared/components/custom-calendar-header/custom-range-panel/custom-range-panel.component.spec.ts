79973513f8f610baa6fe3326d03c38344f9ca6eb
Justin <khanjal@gmail.com>
2023-10-17 14:53:46 -0400

    Added testing updates

--- src/app/shared/components/custom-calendar-header/custom-range-panel/custom-range-panel.component.spec.ts
+++ src/app/shared/components/custom-calendar-header/custom-range-panel/custom-range-panel.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomRangePanelComponent } from './custom-range-panel.component';
+import { DateAdapter } from '@angular/material/core';
+import { MatDateRangePicker } from '@angular/material/datepicker';
 
 describe('CustomRangePanelComponent', () => {
   let component: CustomRangePanelComponent<any>;
@@ -8,7 +10,9 @@ describe('CustomRangePanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomRangePanelComponent ]
+      declarations: [ CustomRangePanelComponent ],
+      imports: [  ],
+      providers: [ DateAdapter, MatDateRangePicker ]
     })
     .compileComponents();
 
