2488c1a8d382bee0f78e8279c44006ed5ab213a0
Justin <khanjal@gmail.com>
2023-12-13 18:12:06 -0500

    Load Modal files

---
+++ src/app/shared/components/load-modal/load-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-modal',
+  templateUrl: './load-modal.component.html',
+  styleUrls: ['./load-modal.component.scss']
+})
+export class LoadModalComponent {
+
+}
