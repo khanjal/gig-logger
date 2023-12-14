246145efd80c16b36ee9e8ec4619afa7058d25a6
Justin <khanjal@gmail.com>
2023-12-14 10:24:55 -0500

    Added load modal to quick component.

--- src/app/shared/components/load-modal/load-modal.component.ts
+++ src/app/shared/components/load-modal/load-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { SpreadsheetService } from '@services/spreadsheet.service';
+import { map, timer } from 'rxjs';
+import { TimerService } from '@services/timer.service';
 
 @Component({
   selector: 'app-load-modal',
@@ -6,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./load-modal.component.scss']
 })
 export class LoadModalComponent {
+    currentTime = 0;
+    divMessages = "";
 
+    constructor(public dialogRef: MatDialogRef<LoadModalComponent>,
+        private _sheetService: SpreadsheetService,
+        private _timerService: TimerService) { }
+
+    async ngOnInit(): Promise<void> {
+        let time = 0;
+
+        this.startTimer();
+
+        time = this.currentTime;
+        this.appendToTerminal("Starting Lambda");
+        await this._sheetService.warmUpLambda();
+        this.appendToTerminal(`Lambda Started In ${this.currentTime - time}s`);
+
+        time = this.currentTime;
+        this.appendToTerminal("Loading Spreadsheet Data");
+        await this._sheetService.loadSpreadsheetData();
+        this.appendToTerminal(`Spreadsheet Data Loaded In ${this.currentTime - time}s`);
+
+        this.appendToTerminal("Modal Closing In 5s");
+
+        await this._timerService.delay(5000);
+        this.dialogRef.close(true);
+    }
+
+    appendToTerminal(text: string) {
+        this.divMessages = `${this.divMessages}<p>${text}</p>`
+    }
+
+    startTimer() {
+        timer(1000, 1000)
+            .pipe(
+                map((x: number) => {
+                    return x;
+                })
+            )
+            .subscribe(t => this.currentTime = t);
+    }
 }
