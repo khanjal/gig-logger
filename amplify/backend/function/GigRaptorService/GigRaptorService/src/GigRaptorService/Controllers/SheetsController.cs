b9a8597f6ddf86cc024d98647b0c7303094cf9a8
Justin <khanjal@gmail.com>
2024-09-02 23:50:01 -0400

    Replaced backend. Added lambda update batch file.

---
+++ amplify/backend/function/GigRaptorService/GigRaptorService/src/GigRaptorService/Controllers/SheetsController.cs
@@ -0,0 +1,76 @@
+﻿using GigRaptorLib.Entities;
+using GigRaptorService.Business;
+using GigRaptorService.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace GigRaptorService.Controllers;
+
+[Route("[controller]")]
+public class SheetsController : ControllerBase
+{
+    private ISheetManager _sheetmanager;
+
+    private void InitializeSheetmanger()
+    {
+        // TODO: Make this a middleware check too.
+        if (!Request.Headers.TryGetValue("SheetId", out StringValues headerValues))
+        {
+            throw new Exception("SheetId must be provided.");
+        }
+
+        _sheetmanager = new SheetManager(ConfigurationHelper.GetJsonCredential(), headerValues!.ToString().Trim());
+    }
+
+    // GET api/sheets/get
+    [HttpGet("get")]
+    public async Task<SheetEntity?> Get()
+    {
+        InitializeSheetmanger();
+        return await _sheetmanager.GetSheets();
+    }
+
+    // GET api/sheets/get/shifts
+    [HttpGet("get/{sheetName}")]
+    public async Task<SheetEntity?> Get(string sheetName)
+    {
+        InitializeSheetmanger();
+        return await _sheetmanager.GetSheet(sheetName);
+    }
+
+    // GET api/sheets/check
+    [HttpGet("check/")]
+    public async Task<List<MessageEntity>> Check()
+    {
+        InitializeSheetmanger();
+        return await _sheetmanager.CheckSheets();
+    }
+
+    // POST api/sheets/create
+    [HttpPost("create")]
+    public async Task<SheetEntity> Post()
+    {
+        InitializeSheetmanger();
+        return await _sheetmanager.CreateSheet();
+    }
+
+    // POST api/sheets/add
+    [HttpPost("add")]
+    public async Task<SheetEntity> Post([FromBody] SheetEntity sheetEntity)
+    {
+        InitializeSheetmanger();
+        return await _sheetmanager.AddData(sheetEntity);
+    }
+
+    // PUT api/sheets/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/sheets/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
---
+++ amplify/backend/function/GigRaptorService/GigRaptorService/src/GigRaptorService/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace GigRaptorService.Helpers;
+
+public static class ConfigurationHelper
+{
+    private static IConfigurationRoot _configuration = new ConfigurationBuilder().Build(); // TODO: See if there is a better way to handle this.
+    public static void GetConfiguration()
+    {
+        _configuration = new ConfigurationBuilder()
+                            .AddEnvironmentVariables() // For GitHub Action Secrets
+                            .AddUserSecrets(Assembly.GetExecutingAssembly(), true) // For Local User Secrets
+                            .Build();
+    }
+
+    public static Dictionary<string, string> GetJsonCredential()
+    {
+        GetConfiguration();
+
+        var parameters = new Dictionary<string, string>
+        {
+            { "type", _configuration["google_credentials:type"] ?? "" },
+            { "privateKeyId", _configuration["google_credentials:private_key_id"] ?? "" },
+            { "privateKey", _configuration["google_credentials:private_key"] ?? "" },
+            { "clientEmail", _configuration["google_credentials:client_email"] ?? "" },
+            { "clientId", _configuration["google_credentials:client_id"] ?? "" }
+        };
+
+        return parameters;
+    }
+}
