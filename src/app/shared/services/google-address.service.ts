a057960e8f9dfce7b3779994695e3163a42d8170
Justin <khanjal@gmail.com>
2025-04-10 14:11:40 -0400

    Refactored to private and moved public methods to top

--- src/app/shared/services/google-address.service.ts
+++ src/app/shared/services/google-address.service.ts
@@ -6,83 +6,6 @@ import { ElementRef, Injectable } from '@angular/core';
 export class GoogleAddressService {
     place!: any;
     formattedAddress!: string;
-    constructor() { }
-
-    getPlaceId(place: any){
-        return place['place_id'];
-    }
-
-    getFormattedAddress(place: any){
-        return place['formatted_address'];
-    }
-
-    getAddrComponent(place: any, componentTemplate: any) {
-        let result;
-
-        for (let i = 0; i < place.address_components.length; i++) {
-            const addressType = place.address_components[i].types[0];
-            if (componentTemplate[addressType]) {
-            result = place.address_components[i][componentTemplate[addressType]];        
-            return result;
-            }
-        }
-        return;
-    }
-
-    getStreetNumber(place: any) {
-        const COMPONENT_TEMPLATE = { street_number: 'short_name' },
-            streetNumber = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-
-        return streetNumber===undefined?'':streetNumber;
-    }
-
-    getStreet(place: any) {
-        const COMPONENT_TEMPLATE = { route: 'long_name' },
-            street = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return street;
-    }
-
-    getLocality(place: any) {
-        const COMPONENT_TEMPLATE = { locality: 'long_name' },
-            city = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return city;
-    }
-
-    getState(place: any) {
-        const COMPONENT_TEMPLATE = { administrative_area_level_1: 'short_name' },
-            state = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return state;
-    }
-
-    getDistrict(place: any) {
-        const COMPONENT_TEMPLATE = { administrative_area_level_2: 'short_name' },
-            state = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return state;
-    }
-
-    getCountryShort(place: any) {
-        const COMPONENT_TEMPLATE = { country: 'short_name' },
-            countryShort = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return countryShort;
-    }
-
-    getCountry(place:any) {
-        const COMPONENT_TEMPLATE = { country: 'long_name' },
-            country = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return country;
-    }
-
-    getPostCode(place: any) {
-        const COMPONENT_TEMPLATE = { postal_code: 'long_name' },
-            postCode = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return postCode;
-    }
-
-    getPhone(place: any) {
-        const COMPONENT_TEMPLATE = { formatted_phone_number: 'formatted_phone_number' },
-            phone = this.getAddrComponent(place, COMPONENT_TEMPLATE);
-        return phone;
-    }
 
     public getPlaceAutocomplete(inputElement: ElementRef<any>) {
         //@ts-ignore
@@ -102,14 +25,18 @@ export class GoogleAddressService {
             ///this.addressChange.emit(this.formattedAddress);
         });
     }
-    
-    private formatAddress() {
-    this.formattedAddress = this.getFormattedAddress(this.place);
-    let name = this.place['name'];
 
-    if (!this.formattedAddress.startsWith(name)) {
-        this.formattedAddress = `${name}, ${this.formattedAddress}`;
-    }
+    public getAddrComponent(place: any, componentTemplate: any) {
+        let result;
+
+        for (let i = 0; i < place.address_components.length; i++) {
+            const addressType = place.address_components[i].types[0];
+            if (componentTemplate[addressType]) {
+            result = place.address_components[i][componentTemplate[addressType]];        
+            return result;
+            }
+        }
+        return;
     }
 
     public clearAddressListeners(inputElement: ElementRef<any>) {
@@ -118,6 +45,77 @@ export class GoogleAddressService {
         this.removePacContainers();
     }
     
+    public getFormattedAddress(place: any){
+        return place['formatted_address'];
+    }
+
+    private getPlaceId(place: any){
+        return place['place_id'];
+    }
+    private getStreetNumber(place: any) {
+        const COMPONENT_TEMPLATE = { street_number: 'short_name' },
+            streetNumber = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+
+        return streetNumber===undefined?'':streetNumber;
+    }
+
+    private getStreet(place: any) {
+        const COMPONENT_TEMPLATE = { route: 'long_name' },
+            street = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return street;
+    }
+
+    private getLocality(place: any) {
+        const COMPONENT_TEMPLATE = { locality: 'long_name' },
+            city = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return city;
+    }
+
+    private getState(place: any) {
+        const COMPONENT_TEMPLATE = { administrative_area_level_1: 'short_name' },
+            state = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return state;
+    }
+
+    private getDistrict(place: any) {
+        const COMPONENT_TEMPLATE = { administrative_area_level_2: 'short_name' },
+            state = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return state;
+    }
+
+    private getCountryShort(place: any) {
+        const COMPONENT_TEMPLATE = { country: 'short_name' },
+            countryShort = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return countryShort;
+    }
+
+    private getCountry(place:any) {
+        const COMPONENT_TEMPLATE = { country: 'long_name' },
+            country = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return country;
+    }
+
+    private getPostCode(place: any) {
+        const COMPONENT_TEMPLATE = { postal_code: 'long_name' },
+            postCode = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return postCode;
+    }
+
+    private getPhone(place: any) {
+        const COMPONENT_TEMPLATE = { formatted_phone_number: 'formatted_phone_number' },
+            phone = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+        return phone;
+    }
+    
+    private formatAddress() {
+        this.formattedAddress = this.getFormattedAddress(this.place);
+        let name = this.place['name'];
+
+        if (!this.formattedAddress.startsWith(name)) {
+            this.formattedAddress = `${name}, ${this.formattedAddress}`;
+        }
+    }
+
     private removePacContainers() {
         const pacContainers = document.querySelectorAll('.pac-container');
         pacContainers.forEach(container => container.remove());
