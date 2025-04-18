3bcfef83a72f4e5cd11fa67c5b5146ae13f717a9
Justin <khanjal@gmail.com>
2025-04-18 12:11:46 -0400

    Refactored focus scroll and added monthly average

--- src/app/shared/directives/focus-scroll/focus-scroll.directive.ts
+++ src/app/shared/directives/focus-scroll/focus-scroll.directive.ts
@@ -1,20 +1,20 @@
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[focus-scroll]',
   standalone: true
 })
 export class FocusScrollDirective {
+  @Input('focus-scroll-options') scrollOptions: ScrollIntoViewOptions = { behavior: 'smooth', block: 'start', inline: 'nearest' };
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
-  ngOnInit() { }
-
-  @HostBinding('class') 
+  @HostBinding('class')
   elementClass = 'focus-scroll';
 
   @HostListener('focus', ['$event.target']) onFocus() {
-    this.el.nativeElement.scrollIntoView(true);
+    if (this.el.nativeElement) {
+      this.el.nativeElement.scrollIntoView(this.scrollOptions);
+    }
   }
-
 }
