4aa7975a1063388736949cbdba941fe0ac8b9c18
Justin <khanjal@gmail.com>
2025-04-19 07:51:41 -0400

    More focus scroll adjustments. Almost back to original

--- src/app/shared/directives/focus-scroll/focus-scroll.directive.ts
+++ src/app/shared/directives/focus-scroll/focus-scroll.directive.ts
@@ -1,31 +1,17 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Output, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[focus-scroll]',
   standalone: true
 })
 export class FocusScrollDirective {
-  @Input('focus-scroll-options') scrollOptions: ScrollIntoViewOptions = { behavior: 'auto', block: 'start', inline: 'nearest' };
   @Output() scrollComplete: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('focus', ['$event.target']) onFocus() {
-    if (this.el.nativeElement) {
-      const adjustScroll = () => {
-        this.el.nativeElement.scrollIntoView(this.scrollOptions);
-        window.removeEventListener('resize', adjustScroll); // Remove listener after adjustment
-        this.scrollComplete.emit(); // Emit event after scroll completes
-      };
-
-      // Add a resize listener to handle viewport changes caused by the keyboard
-      window.addEventListener('resize', adjustScroll);
-
-      // Fallback: Scroll immediately in case resize doesn't trigger
-      setTimeout(() => {
-        this.el.nativeElement.scrollIntoView(this.scrollOptions);
-        this.scrollComplete.emit(); // Emit event after scroll completes
-      }, 300); // Adjust delay as needed
-    }
+    this.renderer.addClass(this.el.nativeElement, 'focus-scroll');
+    this.el.nativeElement.scrollIntoView(true);
+    this.scrollComplete.emit(); // Emit event after scroll completes
   }
 }
