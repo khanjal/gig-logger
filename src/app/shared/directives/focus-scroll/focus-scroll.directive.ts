e85900582b675cab0f978bc41e65f5a844855203
Justin <khanjal@gmail.com>
2025-04-18 20:15:39 -0400

    Updated focus scroll to instantly scroll and try after keyboard displays

--- src/app/shared/directives/focus-scroll/focus-scroll.directive.ts
+++ src/app/shared/directives/focus-scroll/focus-scroll.directive.ts
@@ -5,7 +5,7 @@ import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angula
   standalone: true
 })
 export class FocusScrollDirective {
-  @Input('focus-scroll-options') scrollOptions: ScrollIntoViewOptions = { behavior: 'smooth', block: 'start', inline: 'nearest' };
+  @Input('focus-scroll-options') scrollOptions: ScrollIntoViewOptions = { behavior: 'auto', block: 'start', inline: 'nearest' };
 
   constructor(private el: ElementRef) {}
 
@@ -13,17 +13,22 @@ export class FocusScrollDirective {
   elementClass = 'focus-scroll';
 
   @HostListener('focus', ['$event.target']) async onFocus() {
+    this.scrollIntoView();
+    await this.delay(300); // Adjust delay as needed
+    this.scrollIntoView();
+  }
+
+  private scrollIntoView() {
     if (this.el.nativeElement) {
       try {
         // Delay to allow keyboard to open
-        await this.delay(300); // Adjust delay as needed
         this.el.nativeElement.scrollIntoView(this.scrollOptions);
       } catch (error) {
         // Fallback: manually scroll to the element's position
         const rect = this.el.nativeElement.getBoundingClientRect();
         window.scrollTo({
           top: rect.top + window.scrollY,
-          behavior: 'smooth'
+          behavior: 'auto'
         });
       }
     }
