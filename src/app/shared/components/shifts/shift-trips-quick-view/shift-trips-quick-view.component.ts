e6842d0c9a77b7f472dcd862ae6c076e1056a8c3
Justin <khanjal@gmail.com>
2025-06-12 22:03:49 -0400

    Fixed close icon some

--- src/app/shared/components/shifts/shift-trips-quick-view/shift-trips-quick-view.component.ts
+++ src/app/shared/components/shifts/shift-trips-quick-view/shift-trips-quick-view.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { ActionEnum } from '@enums/action.enum';
 import { ITrip } from '@interfaces/trip.interface';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatIconButton } from '@angular/material/button';
+import { MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { NgClass } from '@angular/common';
 import { TripsQuickViewComponent } from '@components/trips/trips-quick-view/trips-quick-view.component';
@@ -13,9 +13,8 @@ import { TripsQuickViewComponent } from '@components/trips/trips-quick-view/trip
     // imports: [CommonModule, SharedModule],
     templateUrl: './shift-trips-quick-view.component.html',
     styleUrl: './shift-trips-quick-view.component.scss',
-    standalone: true,
-    imports: [
-        MatIconButton,
+    standalone: true,    imports: [
+        MatMiniFabButton,
         MatIcon,
         NgClass,
         TripsQuickViewComponent,
