539c2e4e80667c66b2f386718f236511968e65cc
Justin <khanjal@gmail.com>
2026-02-05 20:39:22 -0500

    Bump aws-amplify and aws-sdk deps in lockfile (#390)
    Regenerate package-lock.json after upgrading AWS packages. This updates
    several @aws-amplify packages (analytics, api, api-graphql, api-rest,
    auth, core, datastore, notifications, storage) to newer patch/minor
    versions and upgrades many @aws-sdk/@smithy transitive dependencies
    (client-firehose, client-kinesis, client-personalize-events, client-sso,
    core, credential providers, etc.) including node engine bumps for some
    aws-sdk packages (>=20.0.0). Also updates fast-xml-parser to ^5.x and
    refreshes resolved URLs and integrity hashes.
    
    ## Summary
    Short description of the change and the problem it solves.
    
    ## Related issues
    - Fixes: # (if applicable)
    
    ## Type of change
    - [ ] Bugfix
    - [ ] New feature
    - [ ] Documentation update
    - [ ] Chore / CI
    
    ## Checklist
    - [ ] I have added/updated tests where applicable
    - [ ] I have run `npm test` locally and the changes pass
    - [ ] I have linted my code
    
    ## Notes for reviewers
    Any important implementation details, caveats, or areas to focus on.

--- src/app/pages/setup/sheet-quick-view/sheet-quick-view.component.ts
+++ src/app/pages/setup/sheet-quick-view/sheet-quick-view.component.ts
@@ -4,13 +4,14 @@ import { ISpreadsheet } from '@interfaces/spreadsheet.interface';
 import { MatCard, MatCardHeader, MatCardContent } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 import { TruncatePipe } from "@pipes/truncate.pipe";
+import { SheetSourceLabelPipe } from '@pipes/sheet-source-label.pipe';
 
 @Component({
     selector: 'app-sheet-quick-view',
     templateUrl: './sheet-quick-view.component.html',
     styleUrls: ['./sheet-quick-view.component.scss'],
     standalone: true,
-    imports: [MatCard, MatCardHeader, MatIcon, MatCardContent, TruncatePipe]
+    imports: [MatCard, MatCardHeader, MatIcon, MatCardContent, TruncatePipe, SheetSourceLabelPipe]
 })
 export class SheetQuickViewComponent implements OnInit {
   @Input() spreadsheet!: ISpreadsheet;
